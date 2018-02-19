5841b1e475efe05ebeb31c2004c00d6f0895ccce
cassiebayer <cbayer@newvisions.com>
2018-02-19 15:23:14 -0500

    cleaning up file structure
    updated controller and shifting analyses to the RMD

--- Controller.R
+++ Controller.R
@@ -1,8 +1,23 @@
+# refreshenvironment
+
+
+# Set working directory ---------------------------------------------------
 setwd("/Users/cassandrabayer/Desktop/R Scripts/Flaredown-Analysis")
+
+
+# Load Packages -----------------------------------------------------------
+# load basic packages
 library(data.table)
 library(tidyr)
+library(tidyverse)
+
+# visualization packages
 library(ggplot2)
+library(plotly)
+
+# basic stats and prediction
 library(stats)
+library(forecast)
 
 # Model Selection
 library(MASS)
@@ -11,9 +26,10 @@ library(car)
 
 #Dates
 library(zoo)
+library(lubridate)
 
-# Functions
-# Functions    
+
+# Load any custom functions -----------------------------------------------
 rowShift <- function(x, shiftLen = 1L) {
   r <- (1L + shiftLen):(length(x) + shiftLen)
   r[r<1] <- NA
@@ -27,3 +43,19 @@ shift <- function(x, offset = 1, pad = NA) {
   ans[is.na(ans)] <- pad
   return(ans)
 }
+
+maxMissing <- function(x){
+  if(all(is.na(x))){
+    return(NA_real_)
+  } else{
+    return(max(x, na.rm = T))
+  }
+}
+
+
+# Load data  --------------------------------------------------------------
+# Inital loading and basic cleaning
+fd <- data.table(read.csv("fd-export 3.csv", stringsAsFactors = F))
+fd[, trackable_name := tolower(trackable_name)]
+
+
--- migraines.R
+++ migraines.R
@@ -2,59 +2,114 @@
 # # Questions: when do you hit your highest point, for how long, and how frequently
 # Date: 1/3/17
 
+# periodicity
+# weather
+# what condiitons and symptoms is it tracked alongside
+# demographics (male vs female)
+# how long do they track it for?
+# what causes/helps
+
+
+
 # filter for migraines
-fd <- fd_full[grepl(x=trackable_name, pattern="migraine")]
+migraines <- fd[grepl(x=trackable_name, pattern="migraine") & trackable_type %in% c("Symptom", "Condition")]
 
 # find max pain by user  
-fd$trackable_value <- as.numeric(fd$trackable_value)
-fd$checkin_date <- as.Date(fd$checkin_date)
-fd <- fd[trackable_type %in% c("Symptom", "Condition"),
-         maxPain := maxMissing(trackable_value), by = user_id]
+migraines$trackable_value <- as.numeric(migraines$trackable_value)
+migraines$checkin_date <- as.Date(migraines$checkin_date)
 
-stop()
-fd[, .N, by = user_id][order(N)]
-fdTest <- fd[user_id=="QEVuQwEAlNMIH8RXhjZvx6HzoW8iXQ==" & trackable_name == "Ulcerative colitis"]
+# Get the daily max by symptoms so we can collapse on date/user
+migraines[, .N, by = user_id][order(N)]
+migraines <- migraines[, .(dailyMax = max(trackable_value, na.rm = T)),
+                       by = .(user_id, checkin_date, trackable_type)]
 
-ggplot(fd, aes(checkin_date, maxPain)) + xlab("") + ylab("Trackable Value") + geom_point()
+# get max pain of all time logged
+migraines[, maxPain := maxMissing(trackable_value), 
+          by = user_id]
 
-sapply(fdSC, class)
+# get total days tracked by user 
+migraines[,daysLogged := uniqueN(checkin_date), 
+          by = user_id]
 
+# Test
+migrainesTest <- migraines[user_id=="QEVuQwEAPl0naFuNbqRIIMQWovkObQ=="]
+
+# Plots
+
+ggplot(data = migrainesTest, mapping =  aes(as.Date(checkin_date[]),dailyMax)) + xlab("") + ylab("Trackable Value") + geom_point()
+ggplot(migrainesTest,
+       aes(x= checkin_date[month(checkin_date)==6],
+           y=as.integer(dailyMax),
+           color=user_id))+
+  geom_point() +
+  scale_x_date(labels = format("%Y-%m-%d"),
+               breaks = "1 week")
+
+
+# migraines with treatment ------------------------------------------------
+## I only want the days where they took a treatment 
+migrainesTreatment <- fd[trackable_type %in% c("Symptom", "Condition", "Treatment") & grepl(x=trackable_name, pattern="migraine")]
+migrainesTreatmentDates <- migrainesTreatment[trackable_type == "Treatment", .(user_id, checkin_date)]
+migrainesTreatment$trackable_value <- as.integer(migrainesTreatment$trackable_value)
+migrainesTreatment[, averageValue := mean(trackable_value, na.rm = T), 
+                   by = .(user_id, checkin_date)]
+
+migrainesTreatment <- migrainesTreatment[grepl(x=trackable_name, pattern="migraine") & trackable_type %in% c("Symptom", "Condition"),
+                         user_id,
+                         by = .(checkin_date, averageValue)]
+migrainesTreatment <- migrainesTreatment[checkin_date %in% migrainesTreatmentDates$checkin_date]
+nt[migrainesTreatmentDates]
+
+
+
+
+## get the list of days where they took a treatment
+### it would be helpful to know/tag treatments with what condition/symptom they're targetting 
+### (users may not know tho)
+listOfTreatmeents <- migrainesTreatment[trackable_type %in% c("Treatment"), 
+                                        unique(checkin_date),
+                                        by = user_id]
+migrainesTreatment[trackable_type %in% c("Treatment"), 
+                   tookTreatment := 1,
+                   by = .(user_id, checkin_date)]
+
+# Futzing around ----------------------------------------------------------
 # add in missing dates and facet wrap by condition and maybe by month
-fd <- fd[trackable_type %in% c("Symptom", "Condition")]
-fd[, min:= as.POSIXct.Date(min(checkin_date, na.rm = T)), by = user_id]
-fd[, max:= as.POSIXct.Date(max(checkin_date, na.rm = T)), by = user_id]
-fd[, median := median(trackable_value, na.rm = T), by= user_id]
-fd[, q1 := quantile(trackable_value, 0.25, na.rm = T), by = user_id]
-fd[, q13:= quantile(trackable_value, 0.75, na.rm = T), by = user_id]
+migraines <- migraines[trackable_type %in% c("Symptom", "Condition")]
+migraines[, min:= as.POSIXct.Date(min(checkin_date, na.rm = T)), by = user_id]
+migraines[, max:= as.POSIXct.Date(max(checkin_date, na.rm = T)), by = user_id]
+migraines[, median := median(trackable_value, na.rm = T), by= user_id]
+migraines[, q1 := quantile(trackable_value, 0.25, na.rm = T), by = user_id]
+migraines[, q13:= quantile(trackable_value, 0.75, na.rm = T), by = user_id]
 
 #distribution by user-- I want to figure out what constitutes as a flare per user
 
 
 # make a data table of full dates
-min <- min(fd$checkin_date, na.rm = T)
-max <- max(fd$checkin_date, na.rm = T)
+min <- min(migraines$checkin_date, na.rm = T)
+max <- max(migraines$checkin_date, na.rm = T)
 x <- seq.POSIXt(from = as.POSIXct(min), to= as.POSIXct(max),by = "1 day")
 x<-data.table(x)
 
-x <- fd[, .(dates = seq.POSIXt(from = min, to= max ,by = "1 day")), by = user_id]
+x <- migraines[, .(dates = seq.POSIXt(from = min, to= max ,by = "1 day")), by = user_id]
 
 setkey(dt, "id", "wday")
 vals <- c("mon", "tue", "wed", "thu", "fri", "sat", "sun")
 idx <- expand.grid(vals, unique(dt$id))[, 2:1]
 dt[J(idx), allow.cartesian=TRUE]
 
-fd$checkin_date <- as.POSIXct.Date(fd$checkin_date)
+migraines$checkin_date <- as.POSIXct.Date(migraines$checkin_date)
 
-fdDates <- fd[, .(user_id = user_id)]
-fd[, date := seq.POSIXt(from=min_time, to=max_time,by = "1 day"), by = user_id]
+migrainesDates <- migraines[, .(user_id = user_id)]
+migraines[, date := seq.POSIXt(from=min_time, to=max_time,by = "1 day"), by = user_id]
 
-fd[, date := seq.POSIXt(min(fd$checkin_date),max(fd$checkin_date)), by = user_id]
+migraines[, date := seq.POSIXt(min(migraines$checkin_date),max(migraines$checkin_date)), by = user_id]
 dates <- as.Date(as.POSIXct(dates), origin = as.Date(min_time))
 
 # find min date and max date by user
 function(x){
-  fd[user_id==x, 
-     date := seq.POSIXt(min(fd$checkin_date),max(fd$checkin_date))]
+  migraines[user_id==x, 
+            date := seq.POSIXt(min(migraines$checkin_date),max(migraines$checkin_date))]
 }
 
-dfDates <- data.frame(date=seq.POSIXt(min(fd$checkin_date),max(fd$checkin_date),by="day"))
+dmigrainesates <- data.frame(date=seq.POSIXt(min(migraines$checkin_date),max(migraines$checkin_date),by="day"))
